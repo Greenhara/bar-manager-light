d44eaa7b51c2f748e26366b77434d5971262014b
Greenhara <nikitagalkin107@gmail.com>
2025-07-28 08:00:03 +0900

    fix cart reference after clearing

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -29,7 +29,7 @@ export class CartComponent {
 
   clearCart(): void {
     this.cartService.clearCart();
-    this.items = [];
+    this.items = this.cartService.getItems();
   }
 
   checkout(): void {
